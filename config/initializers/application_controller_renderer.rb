078553a5eb74fd1a81a3fd075ced8b5abc6562c0
niwanekochan <pooonda.0607@gmail.com>
2021-09-19 01:40:46 +0000

    mesiterro-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'fb95573218771beefb642d163c0e5ebf7c112f2bd65da64f72634dcf677c8a99113f647d73a8bc0bba9e3f50311ca458eb5cc7a648aa62a24f0d7a03adac9abd'
